95b99e6b9ecce686edfe3b52268459d24c047759
Maxwell Beard <maxwell@maxwells-mbp.home>
2019-08-23 16:54:21 +0100

    second submission

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -125,8 +125,8 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
-zooAnimals.map()
+const lowerCase = zooAnimals.map(item => item.animal_name.toLowerCase());
+
 console.log(lowerCase);
 
 /* Request 3: .filter()
@@ -134,15 +134,19 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const lowerPopulation = [];
+
+const lowerPopulation = zooAnimals.filter(size => size.population < 5); 
+
 console.log(lowerPopulation);
 
-/* Request 4: .reduce() 
+/* Request 4: .reduce()
 
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
+
+const populationTotal = zooAnimals.reduce((sum, animal) => sum + animal.population, 0,);
+
 console.log(populationTotal);
 
 /* 
